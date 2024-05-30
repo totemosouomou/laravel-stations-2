af4d862071ffd106d360b494156d043340ad3dab
totemosouomou <totemosouomou@users.noreply.github.com>
2024-05-30 10:08:51 +0900

    railway4 clear

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PracticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+// Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
+Route::get('/getPractice', [PracticeController::class, 'getPractice']);
 
 Route::get('/', function () {
     return view('welcome');
