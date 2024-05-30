673bbc68e99a6c826d1d68ad130ff84b84021f85
totemosouomou <totemosouomou@users.noreply.github.com>
2024-05-30 11:25:06 +0900

    railway7 clear

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/practice2', [PracticeController::class, 'sample2']);
 Route::get('/practice3', [PracticeController::class, 'sample3']);
 Route::get('/getPractice', [PracticeController::class, 'getPractice']);
 Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/admin/movies', [MovieController::class, 'admin']);
 
 Route::get('/movies', [UserMovieController::class, 'index']);
 Route::get('/movies/{id}', [UserMovieController::class, 'Schedule']);
