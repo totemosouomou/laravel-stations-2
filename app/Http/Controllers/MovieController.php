f8e4b10ca3be8d64c17cdf8f271e3f7d9ce4e517
totemosouomou <totemosouomou@users.noreply.github.com>
2024-05-30 17:48:26 +0900

    railway11 clear

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -12,9 +12,36 @@ class MovieController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $movies = Movie::all();
+        // クエリパラメータを取得
+        $isShowing = $request->query('is_showing');
+        $keyword = $request->query('keyword');
+
+        // クエリビルダーを初期化
+        $query = Movie::query();
+
+        // 公開中のみ
+        if ($isShowing === '1') {
+            $query->where('is_showing', 1);
+
+        // 公開予定のみ
+        } elseif ($isShowing === '0') {
+            $query->where('is_showing', 0);
+        }
+
+        // キーワード検索
+        if ($keyword) {
+            $query->where(function($q) use ($keyword) {
+                $q->where('title', 'like', '%' . $keyword . '%')
+                    ->orWhere('description', 'like', '%' . $keyword . '%');
+            });
+        }
+
+        // クエリを実行して映画リストをページネーション
+        $movies = $query->paginate(20);
+
+        // ビューに渡す
         return view('index', ['movies' => $movies]);
     }
 
