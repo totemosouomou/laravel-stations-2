4c9165091399fd851c23fd48a1f0e1d1bcc891a3
totemosouomou <totemosouomou@users.noreply.github.com>
2024-05-30 12:52:45 +0900

    railway8 clear

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -28,4 +28,35 @@ class MovieController extends Controller
         $movies = Movie::all();
         return view('admin', ['movies' => $movies]);
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255|unique:movies,title',
+            'image_url' => 'required|url',
+            'published_year' => 'required|integer|between:2000,2025',
+            'is_showing' => 'boolean',
+            'description' => 'required|string',
+        ]);
+
+        Movie::create([
+            'title' => $request->title,
+            'image_url' => $request->image_url,
+            'published_year' => $request->published_year,
+            'is_showing' => $request->has('is_showing'),
+            'description' => $request->description,
+        ]);
+
+        return redirect()->back()->with('status', '映画が登録されました！');
+    }
 }
