9d28706a59a11e6655f77e62a9c1e930443b0a69
totemosouomou <totemosouomou@users.noreply.github.com>
2024-05-31 22:12:02 +0900

    railway12 clear

--- database/migrations/2024_05_30_175834_create_genres_table.php
+++ database/migrations/2024_05_30_175834_create_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMoviesTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateMoviesTable extends Migration
      */
     public function up()
     {
-        Schema::create('movies', function (Blueprint $table) {
+        //
+        Schema::create('genres', function (Blueprint $table) {
             $table->id();
-            $table->string('title',255)->unique(); // 映画タイトル（ユニーク制約）
-            $table->text('image_url'); // 画像URL
+            $table->string('name')->unique(); // ジャンル名（ユニーク制約）
             $table->timestamps();
         });
     }
@@ -28,6 +28,7 @@ class CreateMoviesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movies');
+        //
+        Schema::dropIfExists('genres');
     }
 }
